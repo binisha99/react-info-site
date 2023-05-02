847580e7833b6ec504bd8cb1ec6c53eddd17bf2d
binisha <binishabajracharya708@gmail.com>
2023-05-02 10:54:50 +0545

    First Commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar()
+{
+    return (
+        <nav>
+            <section className="left">
+            <img src="./src/assets/reactjs-icon.png"/>
+            <h1>
+                ReactFacts
+            </h1>
+            </section>
+      <h2>React Course - Project 1
+        </h2>
+        </nav>
+       
+    );
+}
+export default Navbar;
